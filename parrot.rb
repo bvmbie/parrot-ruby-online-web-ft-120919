404d7aa31854300cbeca3d2536ac8796419695a0
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-18 12:39:40 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(argument, the_parrot="Squawk!")
-  return "#{the_parrot}"
+def parrot(string="Squawk!")
+  puts "#{string}"
+  "#{string}"
 end
-
-parrot
-
-parrot("Pretty bird!")
