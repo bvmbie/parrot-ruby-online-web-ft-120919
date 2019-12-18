4e80b040bbd40010e731e73d69e905407c2a6eca
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-18 12:37:15 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(the_parrot="Squawk!")
+def parrot(argument, the_parrot="Squawk!")
   puts "#{the_parrot}"
 end
 
 parrot
+
+parrot("Pretty bird!")
